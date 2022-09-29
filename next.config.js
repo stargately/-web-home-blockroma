01488aed2f6f24dec6d3266d49da13ad34f7b93d
Dora Noda <dev@stargately.com>
2022-09-28 23:17:28 -0700

    remove redirect

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      ...["eth", "blocks", "api-gateway", "txs", "txn", "tx"].map(seg => {
-        return {
-          source: `/${seg}/:path*`,
-          destination: `https://app.blockroma.com/${seg}/:path*`,
-          permanent: true,
-        };
-      })
-    ]
-  },
 }
 
 module.exports = nextConfig
